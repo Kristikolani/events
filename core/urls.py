bd4d9bb21400189782daf33ab3f444b6a3a0c7bb
Kristi Kolani <kristikolani@gmail.com>
2022-11-06 14:18:59 +0100

    minor updates on home and about page

--- core/urls.py
+++ core/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import  path
+from django.urls import path
 from events import views as eventsViews
-from accounts import views as accountsViews
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", eventsViews.home, name='home'),
     path("about/", eventsViews.about, name='about'),
-    path("register/", accountsViews.registerPage)
-    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Event
 
 
 def home(request):
-    return render(request, 'base.html')
+    events = Event.objects.all()
+    return render(request, 'home.html', {'events': events})
 
 
 def about(request):
